86d8ac14b59f2796f1a39dae788c90d3e31fb146
linanie <403786672@qq.com>
2018-09-11 21:21:08 +0800

    优化

--- template/src/pages/index/store/store.js
+++ template/src/pages/index/store/store.js
@@ -8,31 +8,14 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     userInfo: {
-      phone: '-', //手机号码
-      flow: '--', //剩余流量
-      balance: '--', //余额
-      totalFlow: '--', //总流量
-      curMeal: '--', //已办套餐
-      lineServiceUrl: 'javascript:;', //客服链接
-      consumeHistory: { //消费记录
-        avg: '--',
-        list: [
-          '--', '--', '--'
-        ]
-      },
-      flowHistory: { //流量记录
-        avg: '--',
-        list: [
-          '--', '--', '--'
-        ]
-      }
     }, //保存用户信息 phone
+    isFromIndex: false, //判断是否是从首页进来的
     lotteryPlateDom : null, // 抽奖盘的实例对象
     dialogOrigin: { //dialog的原始值
       title: '',
       message: '',
-      isShow: false,
       isShowFooter: true,
+      isShow: false,
       footer: {
         confirm: {
           text: '确认'
@@ -48,46 +31,7 @@ export default new Vuex.Store({
     dialog: {
       title: '',
       message: '',
-      isShow: false,phone: '-', //手机号码
-      flow: '--', //剩余流量
-      balance: '--', //余额
-      totalFlow: '--', //总流量
-      curMeal: '--', //已办套餐
-      lineServiceUrl: 'javascript:;', //客服链接
-      consumeHistory: { //消费记录
-        avg: '--',
-        list: [
-          '--', '--', '--'
-        ]
-      },
-      flowHistory: { //流量记录
-        avg: '--',
-        list: [
-          '--', '--', '--'
-        ]
-      }
-    }, //保存用户信息 phone
-    lotteryPlateDom : null, // 抽奖盘的实例对象
-    dialogOrigin: { //dialog的原始值
-      title: '',
-      message: '',
-      isShow: false,
       isShowFooter: true,
-      footer: {
-        confirm: {
-          text: '确认'
-        }
-        // cancel: {
-        //   text: '取消',
-        //   callBack () {
-        //     console.log('取消')
-        //   }
-        // }
-      }
-    },
-    dialog: {
-      title: '',
-      message: '',
       isShow: false,
       footer: {
         confirm: {
@@ -96,7 +40,7 @@ export default new Vuex.Store({
       }
     },
     qrDialog: {
-      title: '关注公众号参与活动',
+      title: '参与活动需先关注公众号',
       message: '',
       preventMaskClose: false, //点击蒙层是否可以关闭
       isShow: false,
